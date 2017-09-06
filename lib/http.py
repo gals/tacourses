483495bea83194f0236ebfb3f5aeca020f2ab27c
Gal Shafrir <galshafrir@gmail.com>
2017-09-06 22:01:10 +0300

    Better HTTPCache, added tests

--- lib/http.py
+++ lib/http.py
@@ -4,7 +4,11 @@
 import os
 from hashlib import md5
 import cPickle as pickle
+from functools import wraps
+import unittest
+import shutil
 import requests
+import httpretty
 
 DEFAULT_CACHE_DIR = "cache"
 
@@ -12,25 +16,30 @@ class HTTPCache(object):
 	"""
 	"""
 
-	def __init__(self, cache_path):
-		self.cache_path = cache_path
+	def __init__(self, cache_dir=DEFAULT_CACHE_DIR):
+		"""
+		"""
+		self.cache_dir = cache_dir
 
 	def __repr__(self):
-		return "<%s path=%r>" % (
+		return "<%s dir=%r>" % (
 			self.__class__.__name__,
-			self.cache_path)
+			self.cache_dir)
 
-	def _hash(self, req):
+	def _hash(self, url, params=None, data=None):
 		"""
 		"""
-		url, params, data = req
-		return md5(url + str(data)).hexdigest()
+		params = params or {}
+		data = data or {}
 
-	def _filename(self, req):
+		key = "".join(map(str, [url, params, data]))
+		return md5(key).hexdigest()
+
+	def _build_cache_path(self, *args, **kwargs):
 		"""
 		"""
 		return os.path.join(
-			self.cache_path, self._hash(req))
+			self.cache_dir, self._hash(*args, **kwargs))
 
 	def _serialize(self, resp):
 		"""
@@ -45,45 +54,63 @@ class HTTPCache(object):
 	def __delitem__(self, req):
 		"""
 		"""
-		os.remove(self._filename(req))
+		if not isinstance(req, tuple):
+			req = (req, )
+
+		os.remove(self._build_cache_path(*req))
 
 	def __getitem__(self, req):
 		"""
 		"""
+		if not isinstance(req, tuple):
+			req = (req, )
+
 		if not req in self:
 			return
 
-		filename = self._filename(req)
-		with open(filename, "rb") as f:
+		path = self._build_cache_path(*req)
+		with open(path, "rb") as f:
 			return self._deserialize(f.read())
 
 	def __setitem__(self, req, resp):
 		"""
 		"""
-		filename = self._filename(req)
-		with open(filename, "wb") as f:
+		if not isinstance(req, tuple):
+			req = (req, )
+
+		if self.cache_dir:
+			if not os.path.isdir(self.cache_dir):
+				os.makedirs(self.cache_dir)
+
+		path = self._build_cache_path(*req)
+		with open(path, "wb") as f:
 			f.write(self._serialize(resp))
 
 	def __contains__(self, req):
 		"""
 		"""
-		return os.path.isfile(self._filename(req)) 
+		if not isinstance(req, tuple):
+			req = (req, )
 
-def default_cache():
-	"""
-	"""
-	return HTTPCache(DEFAULT_CACHE_DIR)
+		return os.path.isfile(
+			self._build_cache_path(*req))
 
 def cached(f):
 	"""
 	"""
-	def inner(url, data=None, params=None, 
-		cache=None, *args, **kwargs):
+	@wraps(f)
+	def inner(
+		url,
+		data=None,
+		params=None, 
+		cache=None,
+		*args,
+		**kwargs):
 		if cache is None:
-			cache = default_cache()
+			cache = HTTPCache()
 
-		data = data or {}
 		params = params or {}
+		data = data or {}
 
 		req = (url, params, data)
 		if req in cache:
@@ -112,18 +139,76 @@ def get(*args, **kwargs):
 	"""
 	return requests.get(*args, **kwargs)
 
-def test():
+class TestHTTPCache(unittest.TestCase):
 	"""
 	"""
-	cache = default_cache()
 
-	r = get("http://ynet.co.il", cache=cache)
-	cached = cache[("http://ynet.co.il", {}, {})]
-	assert cached.status_code == 200
+	@classmethod
+	def setUpClass(cls):
+		"""
+		"""
+		cls._cache = HTTPCache("test_cache")
 
-	r = post("http://ynet.co.il/?a=1", cache=cache)
-	cached = cache[("http://ynet.co.il/?a=1", {}, {})]
-	assert cached.status_code == 200
+		httpretty.register_uri(
+			httpretty.GET,
+			"http://a.com",
+			body="a" * 5)
+
+		httpretty.register_uri(
+			httpretty.POST,
+			"http://b.com",
+			body="b" * 5)
+
+	@classmethod
+	def tearDownClass(cls):
+		"""
+		"""
+		shutil.rmtree(cls._cache.cache_dir)
+
+	def setUp(self):
+		"""
+		"""
+		httpretty.enable()
+
+	def tearDown(self):
+		"""
+		"""
+		httpretty.reset()
+		httpretty.disable()
+
+	def test_1_sends_request_if_not_in_cache(self):
+		"""
+		"""
+		resp = get(
+			"http://a.com",
+			cache=self._cache)
+		self.assertTrue(httpretty.has_request())
+
+		resp = post(
+			"http://b.com",
+			data={"b": 2},
+			cache=self._cache)
+		self.assertTrue(httpretty.has_request())
+
+	def test_2_loads_response_from_cache(self):
+		"""
+		"""
+		resp = get(
+			"http://a.com",
+			cache=self._cache)
+		cached = self._cache["http://a.com"]
+
+		self.assertFalse(httpretty.has_request())
+		self.assertEquals(cached.text, "a" * 5)
+
+		resp = post(
+			"http://b.com",
+			data={"b": 2},
+			cache=self._cache)
+		cached = self._cache[("http://b.com", {}, {"b": 2})]
+
+		self.assertFalse(httpretty.has_request())
+		self.assertEquals(cached.text, "b" * 5)
 
 if __name__ == "__main__":
-	test()
+	unittest.main(verbosity=2)
